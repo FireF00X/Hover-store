dce4a6f9c1dd97c235b2b6f206792dfb45e2a735
Ahmed Elsaied <77579065+FireF00X@users.noreply.github.com>
2023-04-04 19:31:26 +0200

    Update hover-store.js

--- js/hover-store.js
+++ js/hover-store.js
@@ -158,7 +158,7 @@ logInButton.onclick = (e) => {
     } else {
         logInError.innerHTML = "";
         // move to store
-        window.location.href="/mainPage.html";
+        window.location.href="../mainPage.html";
 
     }
 }
